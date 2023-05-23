6ffade287ae0bb99026b5657d704467bc1e77834
jdy0120 <jdy0120120@gmail.com>
2023-05-23 10:28:12 +0900

    create button story

--- doyeonism-library/src/components/button/Button.stories.ts
+++ doyeonism-library/src/components/button/Button.stories.ts
@@ -1,26 +1,22 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Header } from './Header';
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
 
 const meta = {
-  title: 'Example/Header',
-  component: Header,
+  title: "button/Button",
+  component: Button,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
-} satisfies Meta<typeof Header>;
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const LoggedIn: Story = {
-  args: {
-    user: {
-      name: 'Jane Doe',
-    },
-  },
+  args: {},
 };
 
 export const LoggedOut: Story = {};
